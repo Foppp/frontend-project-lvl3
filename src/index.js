3ec4f6db363af4703cc67485f961141e79a472c4
Yura <tt@list.ru>
2021-03-03 09:23:01 +0200

    added confugurations

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import 'jquery';
 import 'popper.js';
 import 'bootstrap';
 import './style.css';
-// import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default (a, b) => a + b;
