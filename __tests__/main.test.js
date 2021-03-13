8127158f57f54005796006c549a1752adfaa5f06
Yura <tt@list.ru>
2021-03-13 22:21:02 +0200

    test added

--- __tests__/main.test.js
+++ __tests__/main.test.js
@@ -1,5 +1,20 @@
-import main from '../src/index.js';
+import path from 'path';
+import { promises as fs } from 'fs';
+import parseXml from '../src/parser.js';
 
-test('testing', () => {
-  expect(main(5, 2)).toBe(7);
+const getFixturePath = (name) => path.join(`${__dirname}/__fixtures__/${name}`);
+
+const fileName = 'rss.xml';
+const src = getFixturePath(fileName);
+
+// let expected;
+
+// beforeAll(async () => {
+//   expected = await fs.readFile(getFixturePath('rss.txt'), 'utf-8');
+// });
+
+test('parser', async () => {
+  const expected = await fs.readFile(getFixturePath('rss.txt'), 'utf-8');
+  const actual = await fs.readFile(src, 'utf-8');
+  expect(parseXml(actual)).toEqual(expected);
 });
