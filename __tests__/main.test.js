78e3d3101928105e576f990da4df270c16210754
Yura <tt@list.ru>
2021-03-15 13:03:10 +0200

    minor changes

--- __tests__/main.test.js
+++ __tests__/main.test.js
@@ -2,8 +2,9 @@ import '@testing-library/jest-dom';
 import path from 'path';
 import nock from 'nock';
 import fs from 'fs';
-import { screen } from '@testing-library/dom';
+import { screen, waitFor } from '@testing-library/dom';
 import testingLibraryUserEvent from '@testing-library/user-event';
+import i18next from 'i18next';
 // import parseXml from '../src/parser.js';
 
 import run from '../src/index.js';
@@ -13,7 +14,7 @@ const userEvent = testingLibraryUserEvent;
 nock.disableNetConnect();
 
 const pathToData = path.join('__tests__', '__fixtures__', 'rss.xml');
-const data = fs.readFileSync(pathToData).toString();
+const data = fs.readFileSync(pathToData, 'utf-8');
 
 let elements;
 
@@ -30,17 +31,29 @@ beforeEach(() => {
 });
 
 test('working process', async () => {
-  expect(screen.queryByText('This is not a valid URL!')).not.toBeInTheDocument();
-  expect(elements.submit).not.toBeDisabled();
+  expect(elements.input).not.toHaveClass('is-invalid');
+  expect(screen.queryByText(i18next.t('errors.url'))).not.toBeInTheDocument();
 
   await userEvent.type(elements.input, 'Hello');
-  nock('http://lorem-rss.herokuapp.com')
+  await userEvent.click(elements.submit);
+
+  expect(screen.queryByText(i18next.t('errors.url'))).toBeInTheDocument();
+  expect(elements.input).toHaveClass('is-invalid');
+
+  await userEvent.clear(elements.input);
+
+  const scope = nock('http://lorem-rss.herokuapp.com')
     .get('/feed')
     .reply(200, data);
 
-  // await userEvent.click(elements.submit);
-  // await waitFor(() => {
-  //   expect(elements.input).toHaveClass('is-invalid');
-  //   expect(screen.queryByText('This is not a valid URL!')).toBeInTheDocument();
-  // });
+  await userEvent.type(elements.input, 'http://lorem-rss.herokuapp.com/feed');
+  await userEvent.click(elements.submit);
+
+  await waitFor(() => {
+    expect(elements.input).not.toHaveClass('is-invalid');
+    expect(screen.queryByText(i18next.t('errors.url'))).not.toBeInTheDocument();
+    expect(screen.queryByText('Lorem ipsum')).toBeInTheDocument();
+  });
+
+  scope.done();
 });
