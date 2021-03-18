fbfc7cf24c6b530732ede08432bdc74f446928cf
Yura <tt@list.ru>
2021-03-18 12:04:35 +0200

    added css

--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@
 import * as yup from 'yup';
 import axios from 'axios';
 // import 'bootstrap/dist/css/bootstrap.min.css';
-// import 'bootstrap';
+import 'bootstrap';
 import './style.css';
+import 'bootstrap/dist/css/bootstrap.css';
 import i18next from 'i18next';
 import resources from './locales';
 import initView from './view.js';
