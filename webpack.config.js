59d078637405b30d9e2fede02db0fdbe7bb7a631
Yura <tt@list.ru>
2021-03-18 10:50:56 +0200

    file fixes3

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-  mode: process.env.NODE_ENV || 'production',
+  mode: process.env.NODE_ENV || 'development',
   entry: './index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
