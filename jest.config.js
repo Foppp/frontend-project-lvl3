c034adecc3697b860c7ab300d9333a44430f0eb3
Yura <tt@list.ru>
2021-03-18 21:43:48 +0200

    resotore working files3

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,8 @@ module.exports = {
     '\\.(css|less)$': '<rootDir>/__mocks__/styleMock.js',
   },
   testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.js$': 'babel-jest',
+    '.+\\.(css|styl|less|sass|scss)$': 'jest-transform-css',
+  },
 };
