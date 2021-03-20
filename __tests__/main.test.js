c342631c2fb2f665cc902cb0a2c73cc8c84bce56
Yura <tt@list.ru>
2021-03-20 14:13:10 +0200

    removed everything

--- __tests__/main.test.js
+++ __tests__/main.test.js
@@ -1,44 +1,44 @@
-import '@testing-library/jest-dom';
-import path from 'path';
-import fs from 'fs';
-import { screen, waitFor } from '@testing-library/dom';
-import testingLibraryUserEvent from '@testing-library/user-event';
-import i18next from 'i18next';
-import run from '../src/index.js';
+// import '@testing-library/jest-dom';
+// import path from 'path';
+// import fs from 'fs';
+// import { screen, waitFor } from '@testing-library/dom';
+// import testingLibraryUserEvent from '@testing-library/user-event';
+// import i18next from 'i18next';
+// import run from '../src/index.js';
 
-const userEvent = testingLibraryUserEvent;
+// const userEvent = testingLibraryUserEvent;
 
-let elements;
+// let elements;
 
-beforeEach(() => {
-  const pathToFixture = path.join('__tests__', '__fixtures__', 'index.html');
-  const initHtml = fs.readFileSync(pathToFixture).toString();
-  document.body.innerHTML = initHtml;
-  run();
+// beforeEach(() => {
+//   const pathToFixture = path.join('__tests__', '__fixtures__', 'index.html');
+//   const initHtml = fs.readFileSync(pathToFixture).toString();
+//   document.body.innerHTML = initHtml;
+//   run();
 
-  elements = {
-    input: screen.getByRole('textbox'),
-    submit: screen.getByRole('button', { selector: '[type="submit"]' }),
-  };
-});
+//   elements = {
+//     input: screen.getByRole('textbox'),
+//     submit: screen.getByRole('button', { selector: '[type="submit"]' }),
+//   };
+// });
 
-test('working process', async () => {
-  expect(elements.submit).not.toBeDisabled();
-  userEvent.type(elements.input, 'wrong-url');
-  userEvent.click(elements.submit);
-  expect(elements.input).toHaveClass('is-invalid');
-  expect(screen.queryByText(i18next.t('errors.url'))).toBeInTheDocument();
+// test('working process', async () => {
+//   expect(elements.submit).not.toBeDisabled();
+//   userEvent.type(elements.input, 'wrong-url');
+//   userEvent.click(elements.submit);
+//   expect(elements.input).toHaveClass('is-invalid');
+//   expect(screen.queryByText(i18next.t('errors.url'))).toBeInTheDocument();
 
-  userEvent.clear(elements.input);
+//   userEvent.clear(elements.input);
 
-  userEvent.type(elements.input, 'http://lorem-rss.herokuapp.com/feed');
-  userEvent.click(elements.submit);
-  expect(elements.submit).toBeDisabled();
+//   userEvent.type(elements.input, 'http://lorem-rss.herokuapp.com/feed');
+//   userEvent.click(elements.submit);
+//   expect(elements.submit).toBeDisabled();
 
-  await waitFor(() => {
-    expect(elements.submit).not.toBeDisabled();
-    expect(screen.queryByText(i18next.t('loadSuccess'))).toBeInTheDocument();
-    const posts = screen.getAllByRole('listitem');
-    expect(posts).toHaveLength(11);
-  });
-});
+//   await waitFor(() => {
+//     expect(elements.submit).not.toBeDisabled();
+//     expect(screen.queryByText(i18next.t('loadSuccess'))).toBeInTheDocument();
+//     const posts = screen.getAllByRole('listitem');
+//     expect(posts).toHaveLength(11);
+//   });
+// });
--- src/index.js
+++ src/index.js
@@ -1,172 +1,175 @@
+const sum = (a, b) => a + b;
+
+export default sum;
 /* eslint-disable object-curly-newline */
 /* eslint-disable no-param-reassign */
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import * as yup from 'yup';
-import axios from 'axios';
-import i18next from 'i18next';
-import resources from './locales';
-import initView from './view.js';
+// import 'bootstrap';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import * as yup from 'yup';
+// import axios from 'axios';
+// import i18next from 'i18next';
+// import resources from './locales';
+// import initView from './view.js';
 
-const normalizeFeedsData = (watchedState, xml) => {
-  const feedTitle = xml.querySelector('channel > title');
-  const feedDescription = xml.querySelector('channel > description');
-  const title = feedTitle.textContent;
-  const description = feedDescription.textContent;
-  const feed = { title, description };
-  watchedState.rssData.feeds.unshift(feed);
-};
+// const normalizeFeedsData = (watchedState, xml) => {
+//   const feedTitle = xml.querySelector('channel > title');
+//   const feedDescription = xml.querySelector('channel > description');
+//   const title = feedTitle.textContent;
+//   const description = feedDescription.textContent;
+//   const feed = { title, description };
+//   watchedState.rssData.feeds.unshift(feed);
+// };
 
-const normalizePostsData = (watchedState, xml, currentTime = null) => {
-  const collectedPosts = [];
-  const items = xml.querySelectorAll('channel > item');
-  items.forEach((item) => {
-    watchedState.rssData.postsCountId += 1;
-    const id = watchedState.rssData.postsCountId;
-    const postTitle = item.querySelector('title');
-    const postDescription = item.querySelector('description');
-    const postLink = item.querySelector('link');
-    const pubDate = item.querySelector('pubDate');
-    const date = pubDate.textContent;
-    const title = postTitle.textContent;
-    const description = postDescription.textContent;
-    const link = postLink.textContent;
-    const post = { id, date, title, description, link };
-    if (currentTime) {
-      if (Date.parse(date) > currentTime) {
-        collectedPosts.unshift(post);
-      }
-    } else {
-      collectedPosts.unshift(post);
-    }
-  });
-  watchedState.rssData.posts.push(...collectedPosts);
-};
+// const normalizePostsData = (watchedState, xml, currentTime = null) => {
+//   const collectedPosts = [];
+//   const items = xml.querySelectorAll('channel > item');
+//   items.forEach((item) => {
+//     watchedState.rssData.postsCountId += 1;
+//     const id = watchedState.rssData.postsCountId;
+//     const postTitle = item.querySelector('title');
+//     const postDescription = item.querySelector('description');
+//     const postLink = item.querySelector('link');
+//     const pubDate = item.querySelector('pubDate');
+//     const date = pubDate.textContent;
+//     const title = postTitle.textContent;
+//     const description = postDescription.textContent;
+//     const link = postLink.textContent;
+//     const post = { id, date, title, description, link };
+//     if (currentTime) {
+//       if (Date.parse(date) > currentTime) {
+//         collectedPosts.unshift(post);
+//       }
+//     } else {
+//       collectedPosts.unshift(post);
+//     }
+//   });
+//   watchedState.rssData.posts.push(...collectedPosts);
+// };
 
-const encodeUrl = (url) => {
-  const hook = 'https://hexlet-allorigins.herokuapp.com/get';
-  const queryParams = `disableCache=true&url=${encodeURIComponent(url)}`;
-  return `${hook}?${queryParams}`;
-};
+// const encodeUrl = (url) => {
+//   const hook = 'https://hexlet-allorigins.herokuapp.com/get';
+//   const queryParams = `disableCache=true&url=${encodeURIComponent(url)}`;
+//   return `${hook}?${queryParams}`;
+// };
 
-const parseXml = (data) => {
-  const parser = new DOMParser();
-  return parser.parseFromString(data, 'application/xml');
-};
+// const parseXml = (data) => {
+//   const parser = new DOMParser();
+//   return parser.parseFromString(data, 'application/xml');
+// };
 
-const requestData = (url) => axios.get(url)
-  .then((response) => response.data.contents);
+// const requestData = (url) => axios.get(url)
+//   .then((response) => response.data.contents);
 
-const loadXml = (watchedState, url) => {
-  const encodedUrl = encodeUrl(url);
-  requestData(encodedUrl).then((data) => {
-    const xmlDoc = parseXml(data);
-    const parsererror = xmlDoc.querySelector('parsererror');
-    if (parsererror) {
-      throw new Error(i18next.t('errors.xml'));
-    }
-    watchedState.form.processError = null;
-    watchedState.form.processState = 'finished';
-    watchedState.rssData.url[url] = Date.now();
-    normalizeFeedsData(watchedState, xmlDoc);
-    normalizePostsData(watchedState, xmlDoc);
-  }).catch((err) => {
-    if (err.request) {
-      watchedState.form.processError = i18next.t('errors.network');
-    } else {
-      watchedState.form.processError = err.message;
-    }
-    watchedState.form.processState = 'failed';
-  });
-};
+// const loadXml = (watchedState, url) => {
+//   const encodedUrl = encodeUrl(url);
+//   requestData(encodedUrl).then((data) => {
+//     const xmlDoc = parseXml(data);
+//     const parsererror = xmlDoc.querySelector('parsererror');
+//     if (parsererror) {
+//       throw new Error(i18next.t('errors.xml'));
+//     }
+//     watchedState.form.processError = null;
+//     watchedState.form.processState = 'finished';
+//     watchedState.rssData.url[url] = Date.now();
+//     normalizeFeedsData(watchedState, xmlDoc);
+//     normalizePostsData(watchedState, xmlDoc);
+//   }).catch((err) => {
+//     if (err.request) {
+//       watchedState.form.processError = i18next.t('errors.network');
+//     } else {
+//       watchedState.form.processError = err.message;
+//     }
+//     watchedState.form.processState = 'failed';
+//   });
+// };
 
-export default () => {
-  const refreshDelay = 5000;
-  const defaultLanguage = 'en';
+// export default () => {
+//   const refreshDelay = 5000;
+//   const defaultLanguage = 'en';
 
-  i18next.init({
-    lng: defaultLanguage,
-    debug: false,
-    resources,
-  });
+//   i18next.init({
+//     lng: defaultLanguage,
+//     debug: false,
+//     resources,
+//   });
 
-  const state = {
-    currentLanguage: 'en',
-    form: {
-      processState: 'filling',
-      processError: null,
-      fields: {
-        url: '',
-      },
-      valid: true,
-      error: null,
-    },
-    rssData: {
-      url: {},
-      feeds: [],
-      posts: [],
-      postsCountId: 0,
-    },
-  };
+//   const state = {
+//     currentLanguage: 'en',
+//     form: {
+//       processState: 'filling',
+//       processError: null,
+//       fields: {
+//         url: '',
+//       },
+//       valid: true,
+//       error: null,
+//     },
+//     rssData: {
+//       url: {},
+//       feeds: [],
+//       posts: [],
+//       postsCountId: 0,
+//     },
+//   };
 
-  const form = document.querySelector('form');
-  const input = document.querySelector('input');
+//   const form = document.querySelector('form');
+//   const input = document.querySelector('input');
 
-  const watchedState = initView(state);
+//   const watchedState = initView(state);
 
-  const validate = (value) => {
-    const schema = yup.object().shape({
-      url: yup
-        .string()
-        .url(i18next.t('errors.url'))
-        .test('doubleUrl', i18next.t('errors.doubleUrl'), (val) => !watchedState.rssData.url[val]),
-    });
-    try {
-      schema.validateSync(value);
-      return null;
-    } catch (e) {
-      return e.message;
-    }
-  };
+//   const validate = (value) => {
+//     const schema = yup.object().shape({
+//       url: yup
+//         .string()
+//         .url(i18next.t('errors.url'))
+//         .test('doubleUrl', i18next.t('errors.doubleUrl'), (val) => !watchedState.rssData.url[val]),
+//     });
+//     try {
+//       schema.validateSync(value);
+//       return null;
+//     } catch (e) {
+//       return e.message;
+//     }
+//   };
 
-  input.addEventListener('input', (e) => {
-    const inputData = e.target.value;
-    watchedState.form.processState = 'filling';
-    watchedState.form.fields.url = inputData;
-  });
+//   input.addEventListener('input', (e) => {
+//     const inputData = e.target.value;
+//     watchedState.form.processState = 'filling';
+//     watchedState.form.fields.url = inputData;
+//   });
 
-  form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const formData = new FormData(e.target);
-    const formUrl = formData.get('url');
-    watchedState.form.processState = 'sending';
-    const error = validate(watchedState.form.fields);
-    if (error) {
-      watchedState.form.valid = false;
-      watchedState.form.error = error;
-      watchedState.form.processState = 'failed';
-    } else {
-      watchedState.form.valid = true;
-      watchedState.form.error = null;
-      loadXml(watchedState, formUrl);
-    }
-  });
+//   form.addEventListener('submit', (e) => {
+//     e.preventDefault();
+//     const formData = new FormData(e.target);
+//     const formUrl = formData.get('url');
+//     watchedState.form.processState = 'sending';
+//     const error = validate(watchedState.form.fields);
+//     if (error) {
+//       watchedState.form.valid = false;
+//       watchedState.form.error = error;
+//       watchedState.form.processState = 'failed';
+//     } else {
+//       watchedState.form.valid = true;
+//       watchedState.form.error = null;
+//       loadXml(watchedState, formUrl);
+//     }
+//   });
 
-  const timerId = setTimeout(function reloadXml() {
-    const urlList = watchedState.rssData.url;
-    if (urlList.length === 0) {
-      clearTimeout(timerId);
-    }
-    Object.entries(urlList).forEach(([url, loadTime]) => {
-      const encodedUrl = encodeUrl(url);
-      requestData(encodedUrl).then((data) => {
-        const xmlDoc = parseXml(data);
-        normalizePostsData(watchedState, xmlDoc, loadTime);
-        watchedState.rssData.url[url] = Date.now();
-      }).catch(() => {
-        watchedState.form.processState = 'failed';
-      });
-    });
-    setTimeout(reloadXml, refreshDelay);
-  }, refreshDelay);
-};
+//   const timerId = setTimeout(function reloadXml() {
+//     const urlList = watchedState.rssData.url;
+//     if (urlList.length === 0) {
+//       clearTimeout(timerId);
+//     }
+//     Object.entries(urlList).forEach(([url, loadTime]) => {
+//       const encodedUrl = encodeUrl(url);
+//       requestData(encodedUrl).then((data) => {
+//         const xmlDoc = parseXml(data);
+//         normalizePostsData(watchedState, xmlDoc, loadTime);
+//         watchedState.rssData.url[url] = Date.now();
+//       }).catch(() => {
+//         watchedState.form.processState = 'failed';
+//       });
+//     });
+//     setTimeout(reloadXml, refreshDelay);
+//   }, refreshDelay);
+// };
