dd96131cbd0e13f66073beab9fbcdddd2ef7dcc6
Yura <tt@list.ru>
2021-03-20 18:26:23 +0200

    webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,15 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const path = require('path');
 
 module.exports = {
   mode: process.env.NODE_ENV || 'development',
   entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-  },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.html',
-      inject: 'body',
+      template: 'index.html',
     }),
   ],
   module: {
     rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-        },
-      },
       {
         test: /\.css$/,
         use: ['style-loader', 'css-loader'],
