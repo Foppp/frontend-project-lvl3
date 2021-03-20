2cc815250db1780598e499363bc64430b5d65763
Yura <tt@list.ru>
2021-03-20 18:14:52 +0200

    removed mocks

--- __mocks__/axios.js
+++
@@ -1,9 +0,0 @@
-import fs from 'fs';
-import path from 'path';
-
-const pathToData = path.join('__tests__', '__fixtures__', 'rss.xml');
-const xmlData = fs.readFileSync(pathToData, 'utf-8');
-
-export default {
-  get: jest.fn(() => Promise.resolve({ data: { contents: xmlData } })),
-};
